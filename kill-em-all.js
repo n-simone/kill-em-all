9ab06584fab7d06d3c7e9511716793668e5e958e
Joshua Branson <jbranso@purdue.edu>
2015-02-21 14:55:51 -0500

    more stuff.

--- kill-em-all.js
+++ kill-em-all.js
@@ -1,11 +1,11 @@
 // $(document).ready(function() {
 
-    function startGame () {
-        alert("hello world");
-    }
+function startGame () {
+    alert("hello world");
+}
 
-    // var c = $("#canvas");
-    // var ctx = c.getContext ("2d");
-    // ctx.fillStyle = "#FF0000";
-    // ctx.fillRect (0,0,150,75);
+var c = document.getElementById("Canvas");
+var ctx = c.getContext ("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect (0,0,150,75);
 // });
