be1aabf3a417be257c9911e46d1acdc1d8547a07
Joshua Branson <jbranso@purdue.edu>
2015-02-21 16:32:45 -0500

    some stuff.

--- kill-em-all.js
+++ kill-em-all.js
@@ -1,5 +1,3 @@
-// $(document).ready(function() {
-
 function startGame () {
     alert("hello world");
 
@@ -61,12 +59,3 @@ setInterval(function() {
     draw();
 }, 1000/FPS);
 
-// var d = document.getElementById("myCanvas");
-// var dtx = d.getContext("2d");
-// dtx.moveTo(0,0);
-// dtx.lineTo(200,100);
-// dtx.stroke();
-
-
-
-// });
