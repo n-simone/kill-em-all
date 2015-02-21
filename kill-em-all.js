16c715d03f228bf7c6797010be68392fd5368b80
Joshua Branson <jbranso@purdue.edu>
2015-02-21 15:08:22 -0500

    more commits!

--- kill-em-all.js
+++ kill-em-all.js
@@ -2,10 +2,25 @@
 
 function startGame () {
     alert("hello world");
+
+    
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.strokeText("Hello World",10,50);
 }
 
-var c = document.getElementById("Canvas");
-var ctx = c.getContext ("2d");
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect (0,0,150,75);
+ctx.fillRect(0,0,150,75);
+
+// var d = document.getElementById("myCanvas");
+// var dtx = d.getContext("2d");
+// dtx.moveTo(0,0);
+// dtx.lineTo(200,100);
+// dtx.stroke();
+
+
+
 // });
