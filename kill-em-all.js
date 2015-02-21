0008fa384db177d31ecd14fa10b1026ba69b9481
n-simone <nsimone@purdue.edu>
2015-02-21 16:18:14 -0500

    fixed the merge

--- kill-em-all.js
+++ kill-em-all.js
@@ -7,42 +7,29 @@ function startGame () {
 
 // VARIABLES //
 
-// frames per second
-var FPS = 30;
+var FPS = 30; // frames per second
 
-// var grav = 0;
+// var grav = 0; // gravity
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+var CANVAS_HEIGHT = c.height;
+var CANVAS_WIDTH = c.width;
+
 var hero = {
-<<<<<<< HEAD
-    x: 150,
-    y: 150,
-    w: 25,
-    h: 25,
-  //speed on the x-axis
-    dx: 2,
-  //speed on the y-axis
-    dy: 3,
-    color: "#FF0000",
-=======
-    x: 150, // horizontal position (left edge)
-    y: 150, // vertical position (top edge)
+    x: 150, // horizontal position (based on left edge)
+    y: 150, // vertical position (based on top edge)
     w: 25, // width
     h: 25, // height
     dx: 2, // horizontal velocity (left: -, right: +)
     dy: 3, // vertical velocity (up: -, down: +)
     color: "#FF0000", // color value // red
->>>>>>> eb6802f31dc780cce562664de4da4460e0f38d0d
     draw: function() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
 }
-
-var CANVAS_HEIGHT = c.height;
-var CANVAS_WIDTH = c.width;
 
 // UPDATE //
 function update() {
